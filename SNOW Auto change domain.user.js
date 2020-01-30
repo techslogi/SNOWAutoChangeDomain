ac8b03864a6acad4ad7b7749dd67c2d43b02e899
Gabriel Vicente Boçon <32103117+techslogi@users.noreply.github.com>
2020-01-30 12:29:51 -0300

    add timeout

--- SNOW Auto change domain.user.js
+++ SNOW Auto change domain.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SNOW Auto change domain
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  With the implementation of domains within SNOW, this aims to automatically change it.
 // @updateURL    https://github.com/techslogi/SNOWAutoChangeDomain/raw/master/SNOW%20Auto%20change%20domain.user.js
 // @downloadURL	 https://github.com/techslogi/SNOWAutoChangeDomain/raw/master/SNOW%20Auto%20change%20domain.user.js
@@ -31,13 +31,19 @@
             }
 
             if((userDomain == "ITSM/TOP/GBP/iGBP/Volvo") && (currentDomain == "string:8eff173ddb422300b4c2fd651d961914")){
-                GM_notification ( {title: 'Domain modified.', text: 'User is internal but domain was external. Updating.', image: 'https://itsmgbpeu.service-now.com/favicon.ico?v=4'} );
+                GM_notification ( {title: 'Domain modified.',
+                                   text: 'User is internal but domain was external. Updating.',
+                                   timeout: 6000,
+                                   image: 'https://itsmgbpeu.service-now.com/favicon.ico?v=4'} );
                 $(domainPicker, window.parent.document).val("string:93945d24134362006f37b6d96144b076");
                 window.parent.angular.element('#domain_picker_select_header').triggerHandler('change');
             }
 
             if((userDomain == "ITSM/TOP/GBP/iGBP/Volvo/Volvo DBS") && (currentDomain == "string:93945d24134362006f37b6d96144b076")){
-                GM_notification ( {title: 'Domain modified.', text: 'User is external but domain was internal. Updating.', image: 'https://itsmgbpeu.service-now.com/favicon.ico?v=4'} );
+                GM_notification ( {title: 'Domain modified.',
+                                   text: 'User is external but domain was internal. Updating.',
+                                   timeout: 6000,
+                                   image: 'https://itsmgbpeu.service-now.com/favicon.ico?v=4'} );
                 $(domainPicker, window.parent.document).val("string:8eff173ddb422300b4c2fd651d961914");
                 window.parent.angular.element('#domain_picker_select_header').triggerHandler('change');
             }
